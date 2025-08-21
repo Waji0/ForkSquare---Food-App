cf844ee68a863962602b090799410d07a159a1fb
wajahat ali <wajiali744@gmail.com>
2025-08-21 17:28:38 +0500

    cookie error fix

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -6,7 +6,8 @@ import type { Response } from "express";
 
 export const generateToken = (res:Response, user:IUserDocument ) => {
     const token = jwt.sign({userId:user._id}, process.env.SECRET_KEY!, {expiresIn:'1d'});
-    res.cookie("token", token, {httpOnly:true, sameSite:'strict', maxAge:24*60*60*1000});
+    // res.cookie("token", token, {httpOnly:true, sameSite:'strict', maxAge:24*60*60*1000});
+    res.cookie("token", token, {httpOnly:true, sameSite: "none", maxAge:24*60*60*1000});
     // res.status(200).json({   // for learning purpose as sangam
     //     success: true,
     //     message: "success",
